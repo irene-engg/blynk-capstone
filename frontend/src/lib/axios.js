c8d516ed5fb2b554850fab89e90b2a2e99239781
irene-engg <irene.engg2011@gmail.com>
2025-04-03 16:11:57 -0400

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: import.meta.mode === "development" ? "http://localhost:5004/api" : "/api",
+	baseURL: import.meta.mode === "development"
+		? "http://localhost:5004/api"
+		: "backend-alb-1894237244.ca-central-1.elb.amazonaws.com",
 	withCredentials: true, 
 });
 
