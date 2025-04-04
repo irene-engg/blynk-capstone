576c54177ab11c84e152eafeacaada4416f808f6
irene-engg <irene.engg2011@gmail.com>
2025-04-04 13:30:52 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -44,6 +44,6 @@ if (process.env.NODE_ENV === "production") {
 
 // Start the server and connect to DB
 app.listen(PORT, '0.0.0.0',() => {
-	console.log("Server is running on http://0.0.0.0:" + PORT");
+	console.log("Server is running on http://0.0.0.0:" + PORT);
 	connectDB();
 });
